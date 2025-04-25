34a2af5a1b466492db24785d99ef0d9257c32862
Aiden McCormack <aiden.mccormack@kitware.com>
2025-04-25 14:30:13 -0400

    Added conditioinal home path logic for linux

--- tools/wheel/wheel_builder/common.py
+++ tools/wheel/wheel_builder/common.py
@@ -12,9 +12,14 @@ import shutil
 import subprocess
 import sys
 import tarfile
+import platform
 
 # Resolve the home directory path.
-home_path = os.getenv('HOME')
+if platform.system() == 'Linux':
+    # Linux always builds in Docker containers so home path is deterministic
+    home_path = '/root'
+else:
+    home_path = os.getenv('HOME')
 
 # Location where most of the build will take place.
 build_root = f'{home_path}/.cache/drake-wheel-build'
