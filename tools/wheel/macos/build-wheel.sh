83073ca96c008bb5e0030771586cb8e2f96bf343
Aiden McCormack <aiden.mccormack@kitware.com>
2025-04-28 19:29:40 -0400

    First round of Matthew's revisions

--- tools/wheel/macos/build-wheel.sh
+++ tools/wheel/macos/build-wheel.sh
@@ -27,23 +27,11 @@ readonly python_prefix="$(brew --prefix python@$python_version)"
 readonly python_executable="$python_prefix/bin/$python"
 
 # -----------------------------------------------------------------------------
-# Clean up from old builds.
-# -----------------------------------------------------------------------------
-
-rm -rf "/tmp/drake-wheel-build/$python"
-rm -rf "/tmp/drake-dist/$python"
-
-if [ -e "/tmp/drake" ]; then
-    echo "Unable to proceed: /tmp/drake exists" \
-         "(left over from a failed build?)" >&2
-    exit 1
-fi
-
-# -----------------------------------------------------------------------------
 # Build and "install" Drake.
 # -----------------------------------------------------------------------------
+"$resource_root/image/provision-build.sh" "$python-"
 
-readonly build_root="/tmp/drake-wheel-build/$python/drake-build"
+readonly build_root="/tmp/drake-wheel-build/drake-build"
 
 mkdir -p "$build_root"
 cd "$build_root"
@@ -71,7 +59,7 @@ cmake "$git_root" \
     -DWITH_USER_LAPACK=OFF \
     -DWITH_USER_ZLIB=OFF \
     -DDRAKE_VERSION_OVERRIDE="${DRAKE_VERSION}" \
-    -DCMAKE_INSTALL_PREFIX="/tmp/drake-dist/$python" \
+    -DCMAKE_INSTALL_PREFIX="/tmp/drake-wheel-build/drake-dist" \
     -DPython_EXECUTABLE="$python_executable"
 make install
 
@@ -91,9 +79,9 @@ find "$build_root" -type d -print0 | xargs -0 chmod u+w
 # "Install" additional tools to build the wheel.
 # -----------------------------------------------------------------------------
 
-ln -nsf "$git_root" "/tmp/drake-wheel-build/drake"
+ln -nsf "$git_root" "/tmp/drake-wheel-build/drake-src"
 
-readonly venv_drake="/tmp/drake-wheel-build/drake/venv"
+readonly venv_drake="/tmp/drake-wheel-build/drake-src/venv"
 
 ln -s \
     "$build_root/bazel-bin/external/drake+/tools/wheel/strip_rpath" \
@@ -107,17 +95,12 @@ ln -s \
 # Build the Drake wheel.
 # -----------------------------------------------------------------------------
 
-mkdir -p "/tmp/drake-wheel-build/$python/wheel"
-
-ln -s "/tmp/drake-dist/$python" \
-      "/tmp/drake"
+mkdir -p "/tmp/drake-wheel-build/drake-wheel"
 
 cp \
     "$resource_root/image/setup.py" \
-    "/tmp/drake-wheel-build/wheel/setup.py"
+    "/tmp/drake-wheel-build/drake-wheel/setup.py"
 
 export DRAKE_VERSION="$1"
 
 "$resource_root/image/build-wheel.sh"
-
-rm "/tmp/drake"
--- tools/wheel/wheel_builder/macos.py
+++ tools/wheel/wheel_builder/macos.py
@@ -11,7 +11,6 @@ import subprocess
 from .common import create_snopt_tgz, die, gripe, wheel_name
 from .common import build_root, resource_root, wheel_root, wheelhouse
 from .common import test_root, find_tests
-from .common import dist_root
 
 from .macos_types import PythonTarget
 
@@ -148,7 +147,7 @@ def build(options):
     environment['DRAKE_VERSION'] = options.version
 
     # Create the snopt source archive (and pass along as an environment var).
-    snopt_tgz = os.path.join(resource_root, 'image', 'snopt.tar.gz')
+    snopt_tgz = os.path.join(build_root, 'snopt', 'snopt.tar.gz')
     environment['SNOPT_PATH'] = snopt_tgz
     _files_to_remove.append(snopt_tgz)
     create_snopt_tgz(snopt_path=options.snopt_path, output=snopt_tgz)
@@ -182,7 +181,6 @@ def build(options):
         os.unlink(wheel_root)
 
     if not options.keep_build:
-        shutil.rmtree(dist_root)
         shutil.rmtree(build_root)
         if options.test:
             shutil.rmtree(test_root)
